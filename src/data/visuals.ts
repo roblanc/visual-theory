e4f87600a05e6436425845dcb728a7da1f41a125
netrebnic <70092629+roblanc@users.noreply.github.com>
2025-10-21 17:07:52 +0300

    feat: Add 'The balloon of self importance' card and detail page

--- src/data/visuals.ts
+++ src/data/visuals.ts
@@ -4,6 +4,7 @@ import visual4 from "@/assets/visual-4.jpg";
 import visual5 from "@/assets/visual-5.jpg";
 import visual6 from "@/assets/visual-6.jpg";
 import writingEvergreen from "@/assets/writing-is-evergreen-technology.png";
+import theBalloonOfSelfImportance from "@/assets/the-balloon-of-self-importance.png";
 
 export const visuals = [
   {
@@ -18,27 +19,7 @@ export const visuals = [
     description: "Writing stores our life lessons and experiences.",
     image: writingEvergreen,
     slug: "writing-is-evergreen-technology",
-    content: `Writing stores our life lessons and experiences.
-
-The printing press and internet have given us access to thousands of years of lived experience.
-
-Even though we interact daily, few of us consider ourselves writers or authors.
-
-It's difficult to be objective about our own experience.
-
-Interesting?
-
-Worthwhile?
-
-Who has the ability to decide?
-
-No one can value it unless you do.
-
-I don't only believe this in theory; I currently live off the cash from chronicling my 10-year design career.
-
-There's no downside to documenting your knowledge.
-
-Best case, you become a better writer; worst case, you create something that impacts others.`,
+    content: `Writing stores our life lessons and experiences.\n\nThe printing press and internet have given us access to thousands of years of lived experience.\n\nEven though we interact daily, few of us consider ourselves writers or authors.\n\nIt's difficult to be objective about our own experience.\n\nInteresting?\n\nWorthwhile?\n\nWho has the ability to decide?\n\nNo one can value it unless you do.\n\nI don't only believe this in theory; I currently live off the cash from chronicling my 10-year design career.\n\nThere's no downside to documenting your knowledge.\n\nBest case, you become a better writer; worst case, you create something that impacts others.`,
   },
   {
     title: "Planting Seeds",
@@ -67,5 +48,12 @@ Best case, you become a better writer; worst case, you create something that imp
     image: visual6,
     slug: "building-blocks",
     content: "",
+  },
+  {
+    title: "The balloon of self importance",
+    description: "Everything begins with awareness. Awareness is cleaning yourself up.",
+    image: theBalloonOfSelfImportance,
+    slug: "the-balloon-of-self-importance",
+    content: `Everything begins with awareness. Awareness is cleaning yourself up. Toxicity requires a heavy clean-up. You will need to go through a purging process and combine that with internal awareness so that you can change your environments.\n\nIf you see that your friends don’t push you forward, consider changing your environment. This will also change the people.\n\nNo expectations.\n\nChanging your environment even for a brief period of time can change that perspective, that space to see things clearly.\n\nConfidence can be beneficial until it is not. There is a breaking point. When confidence is overshadowed by arrogance, a person's actions reveal their true character.\n\nSelf-compassion does not imply absolving yourself of responsibility. It all comes down to having a growth mindset that gives you genuine confidence.`,
   },
 ];
